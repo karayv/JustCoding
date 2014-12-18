05c6d74c26c62650c62c8e679bcdbb7e25c47455
Andrey Karayvansky <akarayvansky@cogniance.com>
2014-12-18 11:47:44 -0500

    Added NIO2 tree walking code to find top alphabetically ordered file names.

---
+++ src/main/java/my/coding/nio2/TreeWalkingTest.java
@@ -0,0 +1,62 @@
+package my.coding.nio2;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.stream.Stream;
+
+public class TreeWalkingTest {
+
+    private Comparator<Path> pathDescCmp = (p1, p2) -> p2.getFileName().toString()
+            .compareTo(p1.getFileName().toString());
+    
+    private Stream<Path> firstAlphaOrdered(Path path, int numberOfFiles) throws IOException {
+        PriorityQueue<Path> queue = new PriorityQueue<>(pathDescCmp);
+        
+        Files.walkFileTree(path, new SortingFileVisitor(numberOfFiles, queue));
+        
+        return queue.stream();
+    }
+
+    private class SortingFileVisitor extends SimpleFileVisitor<Path> {
+        
+        private int numberOfFiles;
+        private PriorityQueue<Path> queue;
+
+        public SortingFileVisitor(int numberOfFiles, PriorityQueue<Path> queue) {
+            this.numberOfFiles = numberOfFiles;
+            this.queue = queue;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            queue.offer(file);
+            
+            if (queue.size() > numberOfFiles) {
+                queue.poll();
+            }
+            
+            return FileVisitResult.CONTINUE;
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        String dirStr = args.length == 0 ? "." : args[0];
+        Path path = Paths.get(dirStr);
+        
+        System.out.println("Walking file tree " + path.toAbsolutePath()
+                + " finding top 5 alphabetically ordered file names.\n");
+        
+        Stream<Path> top5 = new TreeWalkingTest()
+            .firstAlphaOrdered(path, 5);
+        
+        top5.map(p -> p.getFileName().toString()).forEach(System.out::println);
+    }
+
+}
